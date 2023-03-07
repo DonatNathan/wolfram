a9daf7b53122545c2ed0f5cd3e68b9af48f0c8bc
DonatNathan <nathan.donat-filliod@epitech.eu>
2023-03-07 14:33:03 +0100

    [ADD] - Parsing and error handling

--- app/Main.hs
+++ app/Main.hs
@@ -3,21 +3,11 @@ module Main (main) where
 import Lib()
 import System.Environment
 import Data.List()
-
--- SET VALUE FUNCTION
--- setValue :: a -> b -> c -> [binary_name] -> value
--- setValue 0 0 0 (binary_name) = binary_name !! 7
--- setValue 0 0 1 (binary_name) = binary_name !! 6
--- setValue 0 1 0 (binary_name) = binary_name !! 5
--- setValue 0 1 1 (binary_name) = binary_name !! 4
--- setValue 1 0 0 (binary_name) = binary_name !! 3
--- setValue 1 0 1 (binary_name) = binary_name !! 2
--- setValue 1 1 0 (binary_name) = binary_name !! 1
--- setValue 1 1 1 (binary_name) = binary_name !! 0
+import System.Exit(exitWith, ExitCode(ExitFailure))
 
 -- ADD RIGHT STRING FUNCTION
-addInList :: Char -> [Char] -> [Char]
-addInList char (list) = (list) ++ [char]
+-- addInList :: Char -> [Char] -> [Char]
+-- addInList char (list) = (list) ++ [char]
 
 -- DISPLAY LINE
 
@@ -29,17 +19,17 @@ displayLine (leftList) (rightList) = do
 
 -- GET BINARY NAME
 
-intToBin :: Int -> [Int]
+intToBin :: Int -> [Char]
 intToBin 0 = []
-intToBin a | a `mod` 2 == 1 = intToBin (a `div` 2) ++ [1]
-           | a `mod` 2 == 0 = intToBin (a `div` 2) ++ [0]
+intToBin a | a `mod` 2 == 1 = intToBin (a `div` 2) ++ ['*']
+           | a `mod` 2 == 0 = intToBin (a `div` 2) ++ [' ']
 
-addZero :: [Int] -> [Int]
-addZero (list) = if (length list) < 8 then (addZero ((list) ++ [0])) else (list)
+addZero :: [Char] -> [Char]
+addZero (list) = if (length list) < 8 then (addZero ((list) ++ [' '])) else (list)
 
 -- GAME LOOP
 
-myLoop :: [String] -> [Int] -> [Char] -> [Char] -> Integer -> IO ()
+myLoop :: [String] -> [Char] -> [Char] -> [Char] -> Integer -> IO ()
 myLoop (args) (binary) (leftList) (rightList) 20 = putStr ""
 myLoop (args) (binary) (leftList) (rightList) line = do
     displayLine leftList rightList
@@ -47,21 +37,84 @@ myLoop (args) (binary) (leftList) (rightList) line = do
     let newRight = createNewRight binary leftList rightList
     myLoop args binary newLeft newRight (line + 1)
 
-createNewLeft :: [Int] -> [Char] -> [Char] -> [Char]
-createNewLeft (binary) (leftList) (rightList) = leftList
+createNewLeft :: [Char] -> [Char] -> [Char] -> [Char]
+createNewLeft (binary) [] (rightList) = []
+createNewLeft (binary) (l:leftList) (rightList) = createNewLeft binary leftList rightList ++ [setValue l l l binary]
 
-createNewRight :: [Int] -> [Char] -> [Char] -> [Char]
-createNewRight (binary) (leftList) (rightList) = rightList
+createNewRight :: [Char] -> [Char] -> [Char] -> [Char]
+createNewRight (binary) (leftList) [] = []
+createNewRight (binary) (leftList) (r:rightList) = createNewRight binary leftList rightList ++ [setValue r r r binary]
+
+-- TODO Remove "!!"
+-- SET VALUE FUNCTION
+setValue :: Char -> Char -> Char -> [Char] -> Char
+setValue ' ' ' ' ' ' (binary_name) = binary_name !! 7
+setValue ' ' ' ' '*' (binary_name) = binary_name !! 6
+setValue ' ' '*' ' ' (binary_name) = binary_name !! 5
+setValue ' ' '*' '*' (binary_name) = binary_name !! 4
+setValue '*' ' ' ' ' (binary_name) = binary_name !! 3
+setValue '*' ' ' '*' (binary_name) = binary_name !! 2
+setValue '*' '*' ' ' (binary_name) = binary_name !! 1
+setValue '*' '*' '*' (binary_name) = binary_name !! 0
 
 -- PARSE ARGUMENTS
 
--- toDict :: [String] -> [(a, b)]
--- toDict (x:xs) = 
+getRule :: [String] -> Int
+getRule [] = -84
+getRule (i:j:args) = if i == "--rule" then read j :: Int else getRule args
+
+getWindow :: [String] -> Int
+getWindow [] = 80
+getWindow (i:j:args) = if i == "--window" then read j :: Int else getWindow args
+
+getStart :: [String] -> Int
+getStart [] = 0
+getStart (i:j:args) = if i == "--start" then read j :: Int else getStart args
+
+getLines :: [String] -> Int
+getLines [] = -1
+getLines (i:j:args) = if i == "--lines" then read j :: Int else getLines args
+
+getMove :: [String] -> Int
+getMove [] = 0
+getMove (i:j:args) = if i == "--move" then read j :: Int else getMove args
+
+isNumber :: [Char] -> Bool
+isNumber [] = True
+isNumber (i:str) = if i > '9' || i < '0' then False else isNumber str
+
+errorHandlingRule :: Int -> Bool
+errorHandlingRule i = if i < 0 || i > 255 then False else True
+
+errorHandlingWindow :: Int -> Bool
+errorHandlingWindow i = if i < 0 then False else True
+
+errorHandlingStart :: Int -> Bool
+errorHandlingStart i = if i < 0 then False else True
+
+errorHandlingLines :: Int -> Bool
+errorHandlingLines i = if i < -1 then False else True
+
+errorHandlingValues :: Int -> Int -> Int -> Int -> Bool
+errorHandlingValues rule window start lines = if (errorHandlingRule rule) && (errorHandlingWindow window) && (errorHandlingStart start) && (errorHandlingLines lines) then True else False
+
+errorHandling :: [String] -> [String] -> Bool
+errorHandling [] (list) = True
+errorHandling (i:j:args) (list) = if (i `elem` list) && isNumber j then errorHandling args list else False
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let binary = (reverse (addZero (reverse (intToBin 30))))
-    let leftList = []
-    let rightList = ['*']
-    myLoop args binary leftList rightList 0
+    let list = ["--rule", "--window", "--move", "--start", "--lines"]
+    args <- getArgs :: IO [String]
+    if (errorHandling args list) == False then exitWith (ExitFailure 84) else do
+        let leftList = []
+        let rightList = ['*']
+        let rule = getRule args
+        let window = getWindow args
+        let start = getStart args
+        let lines = getLines args
+        let move = getMove args
+        if (errorHandlingValues rule window start lines) == False then exitWith (ExitFailure 84) else do
+            let binary = (reverse (addZero (reverse (intToBin rule))))
+            putStrLn binary
+            myLoop args binary leftList rightList 0
