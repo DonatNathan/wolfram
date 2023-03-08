d5a98d24f442e92a1e7d06dcd4605e15575749fd
DonatNathan <nathan.donat-filliod@epitech.eu>
2023-03-08 16:47:06 +0100

    [FIX] - Window

--- src/Display.hs
+++ src/Display.hs
@@ -8,15 +8,18 @@
 module Display where 
 
 displayVoid :: Int -> IO ()
-displayVoid 0 = putStr ""
+displayVoid number | number <= 0 = putStr ""
 displayVoid number = do
     putChar ' '
     displayVoid (number - 1)
 
 displayLine :: [Char] -> [Char] -> Int -> (Int, Int, Int) -> IO ()
 displayLine (leftList) (rightList) line (window, start, move) = if line < start then putStr "" else do
-    displayVoid (window `div` 2 - (line * 2 - 1) `div` 2 + move)
+    displayVoid (window `div` 2 - (line * 2) `div` 2 + move)
     mapM_ putChar (reverse leftList)
     mapM_ putChar rightList
-    displayVoid (window `div` 2 - (line * 2 - 1) `div` 2)
+    if window `mod` 2 == 0 then 
+        displayVoid (window `div` 2 - (line * 2) `div` 2 - 1)
+    else
+        displayVoid (window `div` 2 - (line * 2) `div` 2)
     putChar '\n'
